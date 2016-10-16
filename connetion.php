31247d621dfe58f53a2da20c664f69374f79a882
19anti91 <19anti91@gmail.com>
2016-10-16 16:11:56 -0400

    fixing conection.php

--- connetion.php
+++ connetion.php
@@ -6,7 +6,7 @@
  * Time: 11:32 AM
  */
 
-$kako = mysqli_connect("localhost", "cbello", "root", "kako");
+$kako = mysqli_connect("localhost", "kakoesca_claudia", "ICKKCk123!", "kakoesca_photos");
 // Check connection
 if (!$kako) {
     die("Connection failed: " . mysqli_connect_error());
