e3332eb1b5747d236c6b8542751b288417b48ca9
Noel <no717490@ucf.edu>
2016-11-09 10:44:19 -0500

    fix looping issue

---
+++ Landing.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once("connetion.php");
+include_once("queries.php");
+$dir = "./images/portfolio/";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Kako Escalona</title>
+
+    <script src="js/default.js"></script>
+
+<link rel="stylesheet" href="assets/style.css">
+
+<script> language();</script>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="150">
+<div class="topbar animated fadeInLeftBig"></div>
+
+
+</body>
+</html>
